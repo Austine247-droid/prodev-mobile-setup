cd8c1335cf6ed5cff3d525b79b3a8c9d25e49ff2
Austine247-droid <ugberaeseoa@gmail.com>
2025-07-13 19:34:49 +0000

    updated

--- prodev-mobile-app-0x00/app/index.tsx
+++ prodev-mobile-app-0x00/app/index.tsx
@@ -1,7 +1,8 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
-  return (
+return (
+  <View style={{ flex: 1 }}>
     <View
       style={{
         flex: 1,
@@ -9,7 +10,41 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text> Entry Screen - Awesome</Text>
     </View>
-  );
+
+    <View>
+      <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+      <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+      <Text style={styles.smallText}>ALX is awesome</Text>
+    </View>
+  </View>
+);
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#90caf9",
+  },
+  largeText: {
+    fontSize: 30,
+    color: "#f44336",
+    marginBottom: 5,
+    fontWeight: "700",
+    fontVariant: ["small-caps"],
+  },
+  mediumText: {
+    fontSize: 20,
+    color: "#9c27b0",
+    marginBottom: 10,
+    fontWeight: "500",
+    textAlign: "right",
+  },
+  smallText: {
+    fontSize: 15,
+    color: "#2196f3",
+    fontWeight: "400",
+    textAlign: "center",
+  },
+});
